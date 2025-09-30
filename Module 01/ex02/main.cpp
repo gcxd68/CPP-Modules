0e11eaf7fba633639ae91c2bc54cd47c317d0a05
gcxd68 <94600257+gcxd68@users.noreply.github.com>
2025-09-30 13:47:22 +0200

    Add files via upload

---
+++ Module 01/ex02/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdosch <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/29 13:59:14 by gdosch            #+#    #+#             */
+/*   Updated: 2025/09/29 13:59:15 by gdosch           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <iostream>
+# include <string>
+
+int main( void ) {
+	const std::string string = "HI THIS IS BRAIN";
+	const std::string* stringPTR = &string;
+	const std::string& stringREF = string;
+
+	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+	std::cout << "Value of the string variable: " << string << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+}
