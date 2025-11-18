830a082dc8eb7bc1f87a49a6a8f508edb0c7374d
gcxd68 <94600257+gcxd68@users.noreply.github.com>
2025-11-18 14:43:32 +0100

    Add files via upload

---
+++ Module 05/ex00/Bureaucrat.cpp
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdosch <gdosch@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/18 12:58:02 by gdosch            #+#    #+#             */
+/*   Updated: 2025/11/18 12:58:02 by gdosch           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+// Default constructor
+Bureaucrat::Bureaucrat(void) : _name("Default"), _grade(150) {
+	std::cout << "Bureaucrat default constructor called" << std::endl;
+}
+
+// Parameterized constructor
+Bureaucrat::Bureaucrat(const std::string& name, int grade) : _name(name) {
+	std::cout << "Bureaucrat parameterized constructor called" << std::endl;
+	if (grade < 1)
+		throw GradeTooHighException();
+	if (grade > 150)
+		throw GradeTooLowException();
+	this->_grade = grade;
+}
+
+// Copy constructor
+Bureaucrat::Bureaucrat(const Bureaucrat& other) : _name(other._name), _grade(other._grade) {
+	std::cout << "Bureaucrat copy constructor called" << std::endl;
+}
+
+// Copy assignment operator
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other) {
+	std::cout << "Bureaucrat copy assignment operator called" << std::endl;
+	if (this != &other)
+		this->_grade = other._grade;
+	return *this;
+}
+
+// Destructor
+Bureaucrat::~Bureaucrat(void) {
+	std::cout << "Bureaucrat destructor called" << std::endl;
+}
+
+// Accessor(s)
+const std::string&	Bureaucrat::getName(void) const {
+	return this->_name;
+}
+
+unsigned int	Bureaucrat::getGrade(void) const {
+	return this->_grade;
+}
+
+// Core method(s)
+void	Bureaucrat::incrementGrade(void) {
+	if (_grade <= 1)
+		throw GradeTooHighException();
+	std::cout << "Incrementing " << this->_name << "'s grade" << std::endl;
+	this->_grade--;
+}
+
+void	Bureaucrat::decrementGrade(void) {
+	if (_grade >= 150)
+		throw GradeTooLowException();
+	std::cout << "Decrementing " << this->_name << "'s grade" << std::endl;
+	this->_grade++;
+}
+
+// Exception implementation(s)
+const char*	Bureaucrat::GradeTooHighException::what() const throw() {
+	return "Grade is too high!";
+}
+
+const char*	Bureaucrat::GradeTooLowException::what() const throw() {
+	return "Grade is too low!";
+}
+
+// Insertion operator overload
+std::ostream&	operator<<(std::ostream& out, const Bureaucrat& bureaucrat) {
+	out << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade();
+	return out;
+}
---
+++ Module 05/ex00/Bureaucrat.hpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdosch <gdosch@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/18 12:58:04 by gdosch            #+#    #+#             */
+/*   Updated: 2025/11/18 12:58:04 by gdosch           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <string>
+# include <iostream>
+# include <exception>
+
+class Bureaucrat {
+
+	private:
+
+		const std::string	_name;
+		unsigned int		_grade;
+
+	public:
+
+		// Default constructor
+		Bureaucrat(void);
+
+		// Parameterized constructor
+		Bureaucrat(const std::string& name, int grade);
+
+		// Copy constructor
+		Bureaucrat(const Bureaucrat& other);
+
+		// Copy assignment operator
+		Bureaucrat& operator=(const Bureaucrat& other);
+
+		// Destructor
+		~Bureaucrat(void);
+
+		// Accessor(s)
+		const std::string&	getName(void) const;
+		unsigned int		getGrade(void) const;
+
+		// Core method(s)
+		void				incrementGrade(void);
+		void				decrementGrade(void);
+
+		// Exception class(es)
+		class GradeTooHighException : public std::exception {
+			public:
+				virtual const char* what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception {
+			public:
+				virtual const char* what() const throw();
+		};
+
+};
+
+// Overload of insertion operator
+std::ostream&	operator<<(std::ostream& out, const Bureaucrat& bureaucrat);
+
+#endif
