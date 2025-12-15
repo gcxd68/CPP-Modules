d7427037ee64ec15a1ec5ee7aa1a89441f4f1a79
gcxd68 <94600257+gcxd68@users.noreply.github.com>
2025-12-15 15:15:30 +0100

    Add files via upload

--- Module 09/ex02/PmergeMe.cpp
+++ Module 09/ex02/PmergeMe.cpp
@@ -6,21 +6,17 @@
 /*   By: gdosch <gdosch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 14:36:39 by gdosch            #+#    #+#             */
-/*   Updated: 2025/12/15 13:03:02 by gdosch           ###   ########.fr       */
+/*   Updated: 2025/12/15 15:12:50 by gdosch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
 static unsigned int jacobsthal(size_t n) {
-	if (n == 0)
-		return 0;
-	if (n == 1)
-		return 1;
-	if (n > 32)
+	if (n > 33)
 		return std::numeric_limits<unsigned int>::max();
-	unsigned long power = 1UL << (n + 1);
-	long sign = (n % 2) ? -1 : 1;
+	unsigned long power = 1UL << n;
+	long sign = ((n - 1) % 2) ? -1 : 1;
 	return static_cast<unsigned int>((power + sign) / 3);
 }
 
@@ -29,14 +25,8 @@ void generateInsertionOrder(size_t n, std::vector<size_t>& order) {
 	if (!n)
 		return;
 	std::vector<size_t> jacob;
-	size_t k = 1;
-	while (true) {
-		size_t jac = jacobsthal(k);
-		if (jac > n)
-			break;
+	for (size_t i = 2, jac; (jac = jacobsthal(i)) <= n; i++)
 		jacob.push_back(jac);
-		k++;
-	}
 	size_t prev = 0;
 	for (size_t i = 0; i < jacob.size(); prev = jacob[i++])
 		for (size_t j = jacob[i]; j > prev; j--)
